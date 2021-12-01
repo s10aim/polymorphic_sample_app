35e3fc747abdb69116e40a06b56efbd9efe27e64
tanahara <s10.vermouth.4869@gmail.com>
2021-12-01 13:48:32 +0900

    Commentモデルを作成し, ポリモーフィック関連付けを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_110111) do
+ActiveRecord::Schema.define(version: 2021_12_01_043136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "url"
