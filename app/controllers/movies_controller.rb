5a677b6b2224ef9c01d7349c694c84ace469fcc0
tanahara <s10.vermouth.4869@gmail.com>
2021-12-02 03:24:27 +0900

    メッセージと動画URLの投稿機能を実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,19 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.order(id: :desc)
+    @movie = Movie.new
+  end
+
+  def create
+    Movie.create!(movie_params)
+    redirect_to movies_path
   end
 
   def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:url)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(id: :desc)
+    @post = Post.new
+  end
+
+  def create
+    Post.create!(post_params)
+    redirect_to posts_path
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
